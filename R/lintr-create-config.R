b5e6bf439556a453547cf775b1a559578a34775f
Taavi Päll <tapa741@gmail.com>
2017-09-13 23:48:34 +0300

    let's try lintr

---
+++ R/lintr-create-config.R
@@ -0,0 +1,10 @@
+
+library(magrittr)
+library(dplyr)
+lintr::lint_package() %>%
+  as.data.frame %>%
+  group_by(linter) %>%
+  tally(sort = TRUE) %$%
+  sprintf("linters: with_defaults(\n    %s\n    NULL\n  )\n",
+          paste0(linter, " = NULL, # ", n, collapse="\n    ")) %>%
+  cat(file = ".lintr")
