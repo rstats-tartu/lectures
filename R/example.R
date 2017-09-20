36c10e2a18e5cb07754f8b508ead246624f25a26
ymaivali <ymaivali@gmail.com>
2017-09-20 17:34:27 +0300

    w

--- R/example.R
+++ R/example.R
@@ -1,28 +1,13 @@
 library(tidyverse)
 
-a <- read_csv("Rc.csv")
-?read_csv
+a <- read_csv("https://raw.githubusercontent.com/rstats-tartu/datasets/master/quiz.csv")
 
 colnames(a) <- c("subject", "years", "supervisor", 
                  "stat course", "data anal", "R prof",
                  "x", "x1", "x2", "x3", "x4", "x5", "x6")
 a <- select(a, -x)
 
-a
-
-summary(a)
-
-a <- mutate(a, ratio_x1_x2=x1/x2)
+a <- mutate(a, diff_x1_x2=x1 - x2)
 
 a$years <- parse_number(a$years)
 
-hist(a$years)
-
-boxplot(a$ratio_x1_x2~a$supervisor)
-
-plot(a$x1, a$x2)
-abline(lm(a$x2~a$x1))
-
-b <- rnorm(10)
-mad(b)
-?mad
