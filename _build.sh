6050ddcc0dcf89c803ebbaaa09557a50f527a795
Taavi Päll <tapa741@gmail.com>
2018-01-11 13:59:45 +0200

    build using render_site

--- _build.sh
+++ _build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+Rscript -e "rmarkdown::render_site(encoding = 'UTF-8')"
+
