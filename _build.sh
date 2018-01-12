36201e89b03aceb8cad825e61e7acd182a8ccd62
Taavi Päll <tapa741@gmail.com>
2018-01-12 12:33:33 +0200

    use bundled fonts

--- _build.sh
+++ _build.sh
@@ -1,25 +1,7 @@
 #!/bin/sh
 
-# ~/.local/share/fonts should be used instead
-FONT_HOME=~/.local/share/fonts
-
-echo "installing fonts at $PWD to $FONT_HOME"
-mkdir -p "$FONT_HOME/adobe-fonts/source-code-pro"
-mkdir -p "$FONT_HOME/huertatipografica/Alegreya"
-# find "$FONT_HOME" -iname '*.ttf' -exec echo '{}' \;
-
-(git clone \
-   --branch release \
-   --depth 1 \
-   'https://github.com/adobe-fonts/source-code-pro.git' \
-   "$FONT_HOME/adobe-fonts/source-code-pro" )
-
-(git clone \
-   --branch master \
-   --depth 1 \
-   'https://github.com/huertatipografica/Alegreya.git' \
-   "$FONT_HOME/huertatipografica/Alegreya" )
-fc-cache -fv "$FONT_HOME"
+echo "installing bundled fonts"
+fc-cache -fv fonts/
 
 echo "rendering site"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
