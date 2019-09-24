d256e1642b0793af3b789bff8f79986c1a2e56bb
Ülo Maiväli <ymaivali@gmail.com>
2019-09-24 19:44:46 +0300

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -7,7 +7,7 @@ cp -r fonts/* ~/.local/share/fonts/
 fc-cache -fv ~/.local/share/fonts/
 
 echo "rendering site"
-# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "rmarkdown::render_site(encoding = 'UTF-8')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+#Rscript -e "rmarkdown::render_site(encoding = 'UTF-8')"
 
